23b67c915b74995d78bb7400cacd7c60987d8707
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-03-09 15:37:24 +0100

    added oh-my-zsh fix

--- base.sh
+++ base.sh
@@ -39,7 +39,7 @@ echo "Installing oh-my-zsh"
 echo "-------------------------------------------------"
 
 mkdir $PWD/zsh && cd $PWD/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh::g' | sed 's:chsh -s .*$::g')"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd ../
 rm -rf zsh
 
