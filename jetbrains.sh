29a6e73c8d37a8e7964824f7770d10613cb8d837
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-03-06 18:59:31 +0100

    Added Jetbrains Installation

---
+++ jetbrains.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+pkgs=(
+  "rustrover"
+  "rustrover-jre"
+  "webstorm"
+  "webstorm-jre"
+  "goland-eap"
+  "goland-eap-jre"
+  "clion-eap"
+  "clion-eap-jre"
+  "intellij-idea-ue-eap"
+)
+
+echo "--- Installing JetBrains Tools ---"
+
+for pkg in ${pkgs[@]}; do
+  echo "-- Installing: $pkg"
+  sudo yay -S --needed --noconfirm ${pkg}
+done
