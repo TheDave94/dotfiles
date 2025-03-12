e16413da64acfaeb2a6a0b02287009373419758c
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-03-12 11:36:50 +0100

    Update Packages

--- jetbrains.sh
+++ jetbrains.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
 pkgs=(
-  "rustrover"
-  "rustrover-jre"
-  "webstorm"
-  "webstorm-jre"
-  "goland-eap"
-  "goland-eap-jre"
-  "clion-eap"
-  "clion-eap-jre"
-  "intellij-idea-ue-eap"
+  "intellij-idea-community-edition"
 )
 
 echo "--- Installing JetBrains Tools ---"
 
 for pkg in ${pkgs[@]}; do
+  echo "-- Installing: $pkg"
+  sudo pacman -S --needed --noconfirm ${pkg}
+done
+
+
+aurpkgs=(
+  "rustrover"
+  "rustrover-jre"
+  "webstorm"
+  "webstorm-jre"
+)
+
+echo "--- Installing JetBrains Tools ---"
+
+for pkg in ${aurpkgs[@]}; do
   echo "-- Installing: $pkg"
   yay -S --needed --noconfirm ${pkg}
 done
