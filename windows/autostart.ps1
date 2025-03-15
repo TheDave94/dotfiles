129090124572f9ab9696af92ae3821f7db6c5ba9
fox <102655383+f0xb17@users.noreply.github.com>
2025-03-15 20:21:25 +0100

    Create autostart.ps1
    Added new autostart script.

---
+++ windows/autostart.ps1
@@ -0,0 +1,17 @@
+# Scoop-Updates ausfÃ¼hren
+scoop list | foreach { scoop update $_.Name }
+
+# Get tmp path
+$TempPath = [System.IO.Path]::GetTempPath()
+
+# Check if path exists
+if (Test-Path $TempPath) {
+    # Remove files and folders
+    Get-ChildItem -Path $TempPath -Recurse -Force | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+
+    Write-Host "Path cleaned: $TempPath"
+} else {
+    Write-Host "Path not found: $TempPath"
+}
+
+exit
